a33f3b8f46dbccb259f3743628de1b87222ed3ef
Kaye148 <kaykysampaio13@gmail.com>
2025-08-26 18:28:44 -0300

    Desafios

---
+++ desafio03.py
@@ -0,0 +1,14 @@
+n1 = int (input("digite um numero: "))
+n2 = int (input("digite um numero: "))
+n3 = int (input("digite um numero: "))
+n4 = int (input("digite um numero: "))
+
+#tipos primitivos
+# int para numeros interios
+# float para numeros reais
+# bool para Verdadeiro ou Falso 
+# str para texto 
+
+soma = n1 + n2 + n3 + n4
+
+print(f"a soma entre {n1}+{n2}+{n3}+{n4} é igual a = {soma}")
