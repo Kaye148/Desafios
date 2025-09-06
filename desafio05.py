93ffd81d99746839f7bb1f56e5b612629a60c2f1
Kaye148 <kaykysampaio13@gmail.com>
2025-09-05 21:29:32 -0300

    Add files via upload

---
+++ desafio05.py
@@ -0,0 +1,9 @@
+import os
+os.system("cls")
+
+numero = int(input("digite um número: "))
+
+sucessor = numero +1
+antecessor = numero -1
+
+print(f"o numero que vem depois é {sucessor} o numero que vem antes é o{antecessor} ")
